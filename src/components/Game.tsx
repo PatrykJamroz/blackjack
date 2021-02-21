89b7a92e57ec6fc566c0b8833f189cd0b81c2b17
Patryk J <jamroz.patryk@gmail.com>
2021-02-21 15:10:27 +0100

    added containers

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,6 +2,7 @@ import useGame from "./useGame";
 // import Card from "./Card";
 import {
   GameContainer,
+  Container,
   TopPanel,
   BotPanel,
   Table,
@@ -14,156 +15,168 @@ import {
   Button,
   BetContainer,
   Input,
-  BetInput,
+  PlayerInput,
+  RoundHistoryContainer,
+  TitlePTag,
+  Wrapper,
+  RoundHistoryRecordPtag,
+  RankContainer,
+  RankRecordLiTag,
+  FaultyBetPTag,
 } from "./StyledComponent";
 
 export default function Game() {
   const game = useGame();
 
   return (
-    <div>
-      <GameContainer>
-        <TopPanel>Dealer hand: {game.dealerCount}</TopPanel>
-        <Table>
-          <Deck>
-            {game.roundState === null ? (
-              <CardDummy src="https://www.researchgate.net/profile/Francisco_Perales2/publication/334204491/figure/fig1/AS:776599045697537@1562167049292/Question-card-Symbol-question-mark-example-card.jpg" />
-            ) : (
-              game.dealerHand.map((card, index) => (
-                <Card src={card.image} key={index} />
-              ))
-            )}
-          </Deck>
-          <GameStatus>
-            <GameStatusPtag>{game.roundResult}</GameStatusPtag>
-            <GameStatusPtag>{game.gameStateText}</GameStatusPtag>
-          </GameStatus>
-          <Deck>
-            {game.roundState === null ? (
-              <CardDummy src="https://www.researchgate.net/profile/Francisco_Perales2/publication/334204491/figure/fig1/AS:776599045697537@1562167049292/Question-card-Symbol-question-mark-example-card.jpg" />
-            ) : (
-              game.playerHand.map((card, index) => (
-                <Card src={card.image} key={index} />
-              ))
-            )}
-          </Deck>
-        </Table>
-        <BotPanel>
-          <p style={{ margin: "0" }}>Player hand: {game.playerCount}</p>
-          <p style={{ margin: "0" }}>Round: {game.roundNo}/5</p>
-          <p style={{ margin: "0" }}>Credit: ${game.creditDisplayVal}</p>
-        </BotPanel>
-        <ControlsContainer>
-          <Button
-            onClick={game.handleHit}
-            disabled={game.actionBtnsDisabled ? true : false}
-          >
-            Hit
-          </Button>
-          <Button
-            onClick={game.handleStand}
-            disabled={game.actionBtnsDisabled ? true : false}
-          >
-            Stand
-          </Button>
-          <Button
-            onClick={game.handleDouble}
-            disabled={game.isDoubleBtnDisabled}
-          >
-            Double
-          </Button>
-        </ControlsContainer>
-        <ControlsContainer>
-          <Button
-            onClick={game.handleNewRound}
-            disabled={
-              game.isRoundBtnDisabled || game.isBetFaulty ? true : false
-            }
-            style={{ backgroundColor: "orange" }}
-          >
-            New Round
-          </Button>
-          <BetContainer>
-            Your bet:
-            <Input
-              name="betInput"
-              type="number"
-              value={game.bet}
-              onChange={game.handleBetChange}
-              disabled={game.isBetInputDisabled}
-              autoFocus
+    <GameContainer>
+      <Wrapper>
+        <Container>
+          <TopPanel>Dealer hand: {game.dealerCount}</TopPanel>
+          <Table>
+            <Deck>
+              {game.roundState === null ? (
+                <CardDummy src="https://www.researchgate.net/profile/Francisco_Perales2/publication/334204491/figure/fig1/AS:776599045697537@1562167049292/Question-card-Symbol-question-mark-example-card.jpg" />
+              ) : (
+                game.dealerHand.map((card, index) => (
+                  <Card src={card.image} key={index} />
+                ))
+              )}
+            </Deck>
+            <GameStatus>
+              <GameStatusPtag>{game.roundResult}</GameStatusPtag>
+              <GameStatusPtag>{game.gameStateText}</GameStatusPtag>
+            </GameStatus>
+            <Deck>
+              {game.roundState === null ? (
+                <CardDummy src="https://www.researchgate.net/profile/Francisco_Perales2/publication/334204491/figure/fig1/AS:776599045697537@1562167049292/Question-card-Symbol-question-mark-example-card.jpg" />
+              ) : (
+                game.playerHand.map((card, index) => (
+                  <Card src={card.image} key={index} />
+                ))
+              )}
+            </Deck>
+          </Table>
+          <BotPanel>
+            <p style={{ margin: "0" }}>Player hand: {game.playerCount}</p>
+            <p style={{ margin: "0" }}>Round: {game.roundNo}/5</p>
+            <p style={{ margin: "0" }}>Credit: ${game.creditDisplayVal}</p>
+          </BotPanel>
+          <ControlsContainer>
+            <Button
+              onClick={game.handleHit}
+              disabled={game.actionBtnsDisabled ? true : false}
+            >
+              Hit
+            </Button>
+            <Button
+              onClick={game.handleStand}
+              disabled={game.actionBtnsDisabled ? true : false}
+            >
+              Stand
+            </Button>
+            <Button
+              onClick={game.handleDouble}
+              disabled={game.isDoubleBtnDisabled}
+            >
+              Double
+            </Button>
+          </ControlsContainer>
+          <ControlsContainer>
+            <Button
+              onClick={game.handleNewRound}
+              disabled={
+                game.isRoundBtnDisabled || game.isBetFaulty ? true : false
+              }
+              style={{ backgroundColor: "#CD5C5C" }}
+            >
+              New round
+            </Button>
+            <BetContainer>
+              Your bet:
+              <Input
+                name="betInput"
+                type="number"
+                value={game.bet}
+                onChange={game.handleBetChange}
+                disabled={game.isBetInputDisabled}
+                autoFocus
+              />
+            </BetContainer>
+          </ControlsContainer>
+          <ControlsContainer>
+            <Button
+              onClick={game.startGame}
+              disabled={/*game.isGameOn ||*/ game.isBetFaulty ? true : false}
+              style={{ backgroundColor: "#5F9EA0" }}
+            >
+              New game
+            </Button>
+            <PlayerInput
+              name="playerInput"
+              type="text"
+              value={game.playerName}
+              onChange={game.handlePlayerNameChange}
+              //disabled={game.isGameOn}
             />
-          </BetContainer>
-        </ControlsContainer>
-        <ControlsContainer>
-          <Button
-            onClick={game.startGame}
-            disabled={/*game.isGameOn ||*/ game.isBetFaulty ? true : false}
-            style={{ backgroundColor: "#5F9EA0" }}
+          </ControlsContainer>
+          <FaultyBetPTag
+            style={{
+              display: game.isBetFaulty ? "block" : "none",
+            }}
           >
-            New game
-          </Button>
-          <BetInput
-            name="playerInput"
-            type="text"
-            value={game.playerName}
-            onChange={game.handlePlayerNameChange}
-            disabled={game.isGameOn}
-          />
-        </ControlsContainer>
-      </GameContainer>
-      <p
-        style={{
-          display: game.isBetFaulty ? "block" : "none",
-          color: "red",
-        }}
-      >
-        Bet must be a number between 1 and {game.credit}!
-      </p>
-      <div></div>
-
-      <div></div>
-
-      <div>
-        <div>
-          <p>Rank</p>
-          <ol>
-            {game.rankSorted.map((obj, index) => {
+            Bet must be a number between 1 and {game.credit}!
+          </FaultyBetPTag>
+        </Container>
+        <RoundHistoryContainer>
+          <TitlePTag>Round History</TitlePTag>
+          <div>
+            {game.roundHistory.map((obj, index) => {
               return (
                 <div key={index}>
-                  <li>
-                    {obj.playerName} - ${obj.credit} - {obj.date}
-                  </li>
+                  <RoundHistoryRecordPtag>
+                    Round: {obj.round}
+                  </RoundHistoryRecordPtag>
+                  <RoundHistoryRecordPtag>
+                    Dealer Score: {obj.dealerCount}
+                  </RoundHistoryRecordPtag>
+                  <RoundHistoryRecordPtag>
+                    Cards:{" "}
+                    {obj.dealerHand.map((array, index) => {
+                      return <span key={index}>{array.code} </span>;
+                    })}
+                  </RoundHistoryRecordPtag>
+                  <RoundHistoryRecordPtag>
+                    Player Score: {obj.playerCount}
+                  </RoundHistoryRecordPtag>
+                  <RoundHistoryRecordPtag>
+                    {" "}
+                    Cards:{" "}
+                    {obj.playerHand.map((array, index) => {
+                      return <span key={index}>{array.code} </span>;
+                    })}
+                  </RoundHistoryRecordPtag>
+                  <hr />
                 </div>
               );
             })}
-          </ol>
-        </div>
-        <div>Round History</div>
-        <div>
-          {game.roundHistory.map((obj, index) => {
+          </div>
+        </RoundHistoryContainer>
+      </Wrapper>
+      <RankContainer>
+        <TitlePTag>Rank</TitlePTag>
+        <ol>
+          {game.rankSorted.map((obj, index) => {
             return (
               <div key={index}>
-                <p>Round: {obj.round}</p>
-                <p>
-                  Player Score: {obj.playerCount} (
-                  {obj.playerHand.map((array, index) => {
-                    return <span key={index}>{array.code} </span>;
-                  })}
-                  )
-                </p>
-                <p>
-                  Dealer Score: {obj.dealerCount} (
-                  {obj.dealerHand.map((array, index) => {
-                    return <span key={index}>{array.code} </span>;
-                  })}
-                  )
-                </p>
+                <RankRecordLiTag>
+                  {obj.playerName} - ${obj.credit} - {obj.date}
+                </RankRecordLiTag>
               </div>
             );
           })}
-        </div>
-      </div>
-    </div>
+        </ol>
+      </RankContainer>
+    </GameContainer>
   );
 }
--- src/components/StyledComponent.ts
+++ src/components/StyledComponent.ts
@@ -1,12 +1,27 @@
 import styled from "styled-components";
 
 const GameContainer = styled.div`
+  margin: 0 auto;
+
+  max-width: 450px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 450px;
+  @media (max-width: 500px) {
+    margin: 0 auto;
+  }
+`;
+
+const Container = styled.div`
   width: 300.5px;
   margin: 0 auto;
+  margin-right: 5px;
 `;
 
 const TopPanel = styled.div`
-  background-color: brown;
+  background-color: #dc143c;
   border-radius: 0.5rem 0.5rem 0 0;
   font-size: 1rem;
   color: white;
@@ -21,7 +36,7 @@ const BotPanel = styled(TopPanel)`
 `;
 
 const Table = styled.div`
-  background-color: green;
+  background-color: #556b2f;
   padding: 5px 0;
 `;
 
@@ -41,7 +56,7 @@ const Deck = styled.div`
 `;
 
 const GameStatus = styled.div`
-  background-color: yellow;
+  background-color: #fff8dc;
   margin: 0 5px 0 5px;
   border-radius: 0.5rem;
 `;
@@ -61,12 +76,13 @@ const ControlsContainer = styled.div`
 
 const BetContainer = styled.div`
   width: 66%;
-  background-color: #7fffd4;
+  background-color: #fff0f5;
   padding: 0;
   font-size: 1rem;
   font-family: Arial;
   font-weight: 600;
   border-radius: 0.5rem;
+  border: solid 0.5px gray;
   align-items: center;
   display: flex;
   justify-content: center;
@@ -75,10 +91,10 @@ const BetContainer = styled.div`
 const Button = styled.button`
   width: 32%;
   border-radius: 0.5rem;
-  border: none;
+  border: solid 0.5px gray;
   font-size: 1rem;
   font-weight: 600;
-  background-color: yellow;
+  background-color: #fff8dc;
   padding: 0.25rem 0;
 `;
 
@@ -89,19 +105,66 @@ const Input = styled.input`
   font-size: 1rem;
 `;
 
-const BetInput = styled.input`
+const PlayerInput = styled.input`
   width: 66%;
-  background-color: #7fffd4;
+  background-color: #fff0f5;
   border-radius: 0.5rem;
-  border: none;
+  border: solid 0.5px gray;
   font-size: 1rem;
   font-weight: 600;
   text-align: center;
   padding: 0;
 `;
 
+const RoundHistoryContainer = styled.div`
+  background-color: #f0ffff;
+  border-radius: 0.5rem;
+  width: 135px;
+  padding: 0 5px;
+  border: solid 0.5px gray;
+`;
+
+const TitlePTag = styled.p`
+  font-size: 1rem;
+  font-weight: 800;
+  font-family: Arial;
+  text-align: center;
+  margin: 0 0 1rem 0;
+`;
+
+const RoundHistoryRecordPtag = styled.p`
+  font-size: 0.75rem;
+  font-family: Arial;
+  margin: 0;
+`;
+
+const RankContainer = styled.div`
+  background-color: #b8860b;
+  border-radius: 0.5rem;
+  padding: 0 5px;
+  max-width: 440px;
+  margin-top: 5px;
+`;
+
+const RankRecordLiTag = styled.li`
+  font-size: 1rem;
+  font-family: Arial;
+  justify-content: center;
+`;
+
+const FaultyBetPTag = styled.p`
+  font-size: 0.75rem;
+  font-family: Arial;
+  margin: 5px 0 0 0;
+  text-align: center;
+  background-color: red;
+  border-radius: 0.5rem;
+  color: white;
+`;
+
 export {
   GameContainer,
+  Container,
   TopPanel,
   Table,
   Card,
@@ -114,5 +177,12 @@ export {
   Button,
   BetContainer,
   Input,
-  BetInput,
+  PlayerInput,
+  RoundHistoryContainer,
+  Wrapper,
+  RoundHistoryRecordPtag,
+  RankContainer,
+  TitlePTag,
+  RankRecordLiTag,
+  FaultyBetPTag,
 };
